6129a2eee31357d9adaa6a5ccdab588239875b12
Praveen Josyula <praveenkumar78952@gmail.com>
2024-08-11 09:35:38 +0530

    Created Reagister route

--- Back-End/UserRoute/User.js
+++ Back-End/UserRoute/User.js
@@ -3,23 +3,27 @@ const express = require("express");
 const userSchema = require("../Models/UserModel.js");
 var router  = express.Router();
 
-router.get("/login",(req,res)=>{
+router.post("/register",(req,res)=>{
     var UserModel = mongoose.model("User",userSchema);
+    var requestHeaders = req.headers;
     var user = new UserModel({
-        name: "tester",
-        email: "test1@db.com",
-        password:  "testPassword"
+        name: requestHeaders.name,
+        email: requestHeaders.email,
+        password:  requestHeaders.password
     });
     
-    user.save().then((res)=>{
-        console.log(res);
+    user.save().then((savedRes)=>{
+        res.status(200).json(savedRes);
     }).catch((err)=>{
-        console.log(err);
+        err.errorResponse.code == 11000 ?
+             res.status(400).json({"Error":"An User with this Email already exits."})
+            : res.status(500).json({"Error":"Soory Unable to Register the user right now. Please visit us again after Some time."})
     })
 });
 
-router.get("/register",(req,res)=>{
-    res.send("Registration. Route");
+router.get("/login",(req,res)=>{
+    var UserModel = mongoose.model("User",userSchema);
+    res.json(UserModel.findOne({email : req.headers.email, password : req.headers.password}))
 })
 
 module.exports = router;
